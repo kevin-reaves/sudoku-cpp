9ac0519cf269c58ab8ef42ad1fa82c686def14c0
Kevin Reaves <kevin82593@hotmail.com>
2019-05-19 09:32:29 -0500

    basic property tests passing, getting ready to randomize input

--- test/sudokuTest.cpp
+++ test/sudokuTest.cpp
@@ -17,7 +17,7 @@ struct sudokuTests : testing::Test{
 /*
  * Checking to make sure the rows in a solved grid are valid
  */
-TEST_F(sudokuTests, rowTests){
+TEST_F(sudokuTests, rowTest){
     solver sudokuSolver(grid);
     sudokuSolver.solveSudoku();
     gridLayout solved = sudokuSolver.returnArray();
@@ -27,11 +27,9 @@ TEST_F(sudokuTests, rowTests){
     // Similar to validRow function from solver/sudoku.cpp
     for(int row=0; row<solved.size(); row++){
         for(int col=0; col<solved[row].size(); col++){
-            // There shouldn't be any zeroes in a solved grid
-            ASSERT_NE(solved[row][col], 0) << "There is a zero in a solved grid\n";
-
+            // There shouldn't be any duplicate numbers in a row
             if(used.find(solved[row][col]) != used.end()){
-                ASSERT_TRUE(false) << "There is a duplicate value in a solved grid\n";
+                ASSERT_TRUE(false) << "There is a duplicate value in a solved grid row\n";
             }
 
             used.insert(solved[row][col]);
@@ -39,6 +37,48 @@ TEST_F(sudokuTests, rowTests){
         // Checking each row for duplicates, can reset the set at the end
         used.clear();
     }
+}
+
+/*
+ * Checking to make sure the rows in a solved grid are valid
+ */
+TEST_F(sudokuTests, columnTest) {
+    solver sudokuSolver(grid);
+    sudokuSolver.solveSudoku();
+    gridLayout solved = sudokuSolver.returnArray();
+
+    std::set<int> used;
+
+    for (int col = 0; col < solved[0].size(); col++) {
+        // Similar to validRow function from solver/sudoku.cpp
+        for (int row = 0; row < solved.size(); row++) {
+                // There shouldn't be any duplicate numbers in a row
+                if(used.find(solved[row][col]) != used.end()) {
+                    ASSERT_TRUE(false) << "There is a duplicate value in a solved grid column\n";
+                }
+                    used.insert(solved[row][col]);
+            }
+            // Checking each row for duplicates, can reset the set at the end
+            used.clear();
+        }
+    }
 
 
+/*
+ * Checking to make sure no invalid numbers (outside 1-N) are in a solved grid
+ * N is defined in solver/sudoku.hpp
+ */
+TEST_F(sudokuTests, validNumberTest){
+    solver sudokuSolver(grid);
+    sudokuSolver.solveSudoku();
+    gridLayout solved = sudokuSolver.returnArray();
+
+    for(int row=0; row<solved.size(); row++){
+        for(int col=0; col<solved[row].size(); col++){
+            // There should only be numbers from 1-N in a grid
+            if(solved[row][col] > N || solved[row][col] < 1){
+                ASSERT_FALSE(true) << "There is a number outside 1-N in a solved grid\n";
+            }
+        }
+    }
 }
